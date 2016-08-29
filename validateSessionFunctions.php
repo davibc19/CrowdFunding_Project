255b7204d36223633f488dc13940db1ffc489185
davibc19 <davibc19@hotmail.com>
2016-08-29 11:10:32 -0300

    Atualização das funções de validação - Comentários adicionados

--- validateSessionFunctions.php
+++ validateSessionFunctions.php
@@ -3,10 +3,13 @@ session_start();
 
 function validateHeader()
 {
+    // Usuário Deslogado
     if (!isset($_SESSION["tipoUsr"]))
     {
         include("header.php");
-    } else if (strcmp ($_SESSION["tipoUsr"], "1") == 0)
+    }
+    // Usuário Gestor Orçamentário
+    else if (strcmp ($_SESSION["tipoUsr"], "1") == 0)
     {
         include("headerGO.php");
     }
